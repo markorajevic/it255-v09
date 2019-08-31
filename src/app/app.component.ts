3bf6e261d29d9b287aab4b58f01b79cc1f6b1cc3
Marko <marko@Macbooks-MacBook-Pro.local>
2019-08-31 17:21:48 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { PostService } from './services/post.service';
+import { Post } from './models/post';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,60 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'it255-v09';
+  public postForm: FormGroup;
+  public posts: Post[] = [];
+  
+  constructor(private _postService: PostService) { 
+    this._postService.getPosts().subscribe((data) => {
+      this.posts = data;
+    })
+    this.initForm();
+  }
+
+  public initForm() {
+    this.postForm = new FormGroup({
+      title: new FormControl('', [
+        Validators.required
+      ]),
+      userId: new FormControl(1, [
+        Validators.required
+      ]),
+      body: new FormControl('', [
+        Validators.required
+      ])
+    });
+  }
+
+  public submitForm() {
+    let title = this.postForm.get('title').value;
+    let userId = this.postForm.get('userId').value;
+    let body = this.postForm.get('body').value;
+    let post = new Post(userId, title,body, 0);
+    this.createPost(post)
+  }
+
+  public getPost(id: number) {
+    this._postService.getPost(id).subscribe((data) => {
+      alert(JSON.stringify(data));
+    })
+  }
+
+  public createPost(post: Post) {
+    this._postService.createPost(post).subscribe((data) => {
+      this.posts.unshift(data);
+    })
+  }
+
+  public deletePost(id: number) {
+    this._postService.deletePost(id).subscribe((data) => {
+      this._removePostFromList(id);
+      alert("Post je obrisan sa servera");
+    })
+  }
+
+  private _removePostFromList(id: number) {
+    let ind = this.posts.findIndex(post => post.id == id);
+    this.posts.splice(ind, 1);
+  }
+
 }
